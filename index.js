25405a38fafd952a40dfbaa4e8a1d3a374f9d696
iwgx <wahabiputra@gmail.com>
2019-04-24 15:21:48 +0700

    Enable get single pokemon data

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/:slug', (request, response) => {
-  response.send(request.params.slug)
+app.get('/:slug', async (request, response) => {
+  const { data } = await axios.get(`https://pokeapi.glitch.me/v1/pokemon/${request.params.slug}`, {
+    Headers: {
+      'User-Agent': 'Regalion (http://regalion.surge.sh, v1.0.0)'
+    }
+  })
+
+  response.send({ data })
 })
 
 app.listen(port, () => {
