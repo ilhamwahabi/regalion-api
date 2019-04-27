b7dc9a6051d2cdd17a132179daaa6447f42dce1b
iwgx <wahabiputra@gmail.com>
2019-04-27 13:50:31 +0700

    Parse redis url first

--- redis.js
+++ redis.js
@@ -1,9 +1,13 @@
 const redis = require('redis')
+const url = require('url')
 
-const port = process.env.REDIS_PORT || 6379
-const host = process.env.REDIS_HOST || '127.0.0.1'
+const redisUrl = url.parse(process.env.REDIS_URL)
 
-const client = redis.createClient(port, host)
+const port = redisUrl.port || 6379
+const host = redisUrl.hostname || '127.0.0.1'
+
+const client = redis.createClient(port, host, { no_ready_check: true })
+client.auth(redisUrl.auth.split(":")[1])
 
 client.on('connect', () => {
   console.log('Redis client connected')
